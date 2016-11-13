206efaf324b5edc9d61921239b3f3717ee3a2035
LemonaInc <jaxonstevens@lemonainc.com>
2016-11-12 21:41:40 -0800

    MineSweeper
    MineSweeper

---
+++ App.js
@@ -0,0 +1,49 @@
+// Copyright (C) 2016 Jaxon Stevens
+
+// what it/does
+
+<script> let name = "super game";
+document.querySelector("#game-title").innerHTML = name;
+</script>
+
+
+// Let name =  Super MindSweeper";
+
+class App {
+constructor() {
+
+    // Build the app
+
+    // Showed the splash screen
+
+    this._showSplash();
+
+  // Handeled the play now button-collapse
+
+
+
+ // Build the app map
+
+
+  // Setup Handlers
+
+run()  {
+// Run the game
+let name = "Super MindSweeper"
+document.querySelector("game title").innerHTML = name;
+
+
+}
+
+_showSplash() {
+
+
+
+  let screenselection = document.querySelector "SplashSelector"
+}
+
+
+
+  }
+
+}
